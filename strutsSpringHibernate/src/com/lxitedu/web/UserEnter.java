ca43e514416e141fa9bcba8cdb88c439192f26f5
liguilong <53600461@qq.com>
2011-08-10 16:46:03 +0800

    李贵龙

---
+++ strutsSpringHibernate/src/com/lxitedu/web/UserEnter.java
@@ -0,0 +1,12 @@
+package com.lxitedu.web;
+
+public class UserEnter {
+	public String execute(){
+		System.out.println("UserEnter.execute()");
+		return "success";
+	}
+	public String update(){
+		System.out.println("UserEnter.update()");
+		return "success";
+	}
+}
---
+++ strutsSpringHibernate/src/com/lxitedu/web/UserRequest.java
@@ -0,0 +1,8 @@
+package com.lxitedu.web;
+
+public class UserRequest {
+	public String execute() {
+		System.out.println("UserRequest.execute()");
+		return "success";
+	}
+}
